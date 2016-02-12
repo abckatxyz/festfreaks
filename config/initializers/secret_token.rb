6e96ca46971af89dc03d5e7f059e4bc5fd2625b1
Kat Maribao <kmaribao@gmail.com>
2016-02-12 22:15:20 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Festfreaks::Application.config.secret_key_base = '83db284f42de47d2b46efaeaa817d0b1adb727239c505e54496f87f74df9bc8bd1a48d25f69bd2a24357dc1ba53c1392b7d164ec1a81c24d4a6e5e9fc1196a79'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Festfreaks::Application.config.session_store :cookie_store, key: '_festfreaks_session'
