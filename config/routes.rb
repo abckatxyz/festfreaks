1a5dd49bcd87195f2de30e56740d53fe9477de9d
Kat Maribao <kmaribao@gmail.com>
2016-02-12 22:34:52 +0000

    some styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Festfreaks::Application.routes.draw do
+  root 'festivals#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
