1e4dffd059771437f6e991008cb30f17c8e88382
Kat Maribao <kmaribao@gmail.com>
2016-03-11 02:57:49 +0000

    simple form

--- app/controllers/festivals_controller.rb
+++ app/controllers/festivals_controller.rb
@@ -1,4 +1,10 @@
 class FestivalsController < ApplicationController
   def index
+    @review = Review.first 
   end
+
+  def new
+    @review = Review.new
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Festfreaks::Application.routes.draw do
   root 'festivals#index'
+
+  resources :festivals
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
